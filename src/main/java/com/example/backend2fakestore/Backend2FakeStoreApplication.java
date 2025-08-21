7b39715b185295aa6693718330a36fa89f5073e1
PauliLinde <jus.paulina@gmail.com>
2025-08-21 11:10:24 +0200

    Initial commit

---
+++ src/main/java/com/example/backend2fakestore/Backend2FakeStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend2fakestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend2FakeStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Backend2FakeStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend2fakestore/Backend2FakeStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend2fakestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend2FakeStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
