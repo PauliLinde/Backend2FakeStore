eea96a2c3f8451a1d68a65363f6682bfa63967c3
LEB-Atheel <ado@lebsystem.se>
2025-08-31 20:57:44 +0200

    Intigration test done

--- src/test/java/com/example/backend2fakestore/FakestoreTestIT.java
+++ src/test/java/com/example/backend2fakestore/FakestoreTestIT.java
@@ -3,22 +3,35 @@ package com.example.backend2fakestore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.assertj.core.api.Assertions;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.util.Assert;
+
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 public class FakestoreTestIT {
+
+
 
     @Test
     void shouldReturnAttribute (){
     String url = "https://fakestoreapi.com/products";
     TestRestTemplate rt = new TestRestTemplate();
     String json = rt.getForObject(url, String.class);
-    assertNotNull(json.contains("title"));
+
+    assertNotNull(json);
+
+    assertTrue(json.contains("id"));
+    assertTrue(json.contains("price"));
+    assertTrue(json.contains("title"));
+    assertTrue(json.contains("category"));
+    assertTrue(json.contains("description"));
+    assertTrue(json.contains("image"));
+    assertTrue(json.contains("count"));
+    assertTrue(json.contains("rating"));
+    assertTrue(json.contains("rate"));
 
     }
 
